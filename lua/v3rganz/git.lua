44be6b641d09f2c2d2a04eedbcd7bc0512bb1ac6
evgenijdedov <evgggd@gmail.com>
2023-05-17 00:06:40 +0300

    add git blame line

--- lua/v3rganz/git.lua
+++ lua/v3rganz/git.lua
@@ -3,4 +3,12 @@ if not status_ok then
     return
 end
 
-git_signs.setup()  -- pass some configuration later on
+git_signs.setup({
+    current_line_blame = true,
+    current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol',
+        delay = 0
+    },
+    current_line_blame_formatter = '<committer> <committer_time> - <summary>'
+})
