c2acfcea569e0b07291d46c3d085e32f9add53b0
evgenijdedov <evgggd@gmail.com>
2023-05-17 00:03:54 +0300

    add some string operations to util

--- lua/v3rganz/util.lua
+++ lua/v3rganz/util.lua
@@ -21,4 +21,18 @@ function M.match_path(patterns, path)
     return false
 end
 
+function M.str_startswith(str, start)
+    return string.sub(str, 1, string.len(start)) == start
+end
+
+function M.str_join(str_array, sep)
+    local res = str_array[1]
+    for index, value in ipairs(str_array) do
+        if index ~= 1 then
+            res = res .. sep .. value
+        end
+    end
+    return res
+end
+
 return M
