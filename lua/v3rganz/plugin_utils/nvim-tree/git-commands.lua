02d1a753e9eb0a554c3c5b291e75b591925fedb5
evgenijdedov <evgggd@gmail.com>
2023-05-17 21:41:32 +0300

    fix nvimtree git commands

--- lua/v3rganz/plugin_utils/nvim-tree/git-commands.lua
+++ lua/v3rganz/plugin_utils/nvim-tree/git-commands.lua
@@ -1,32 +1,25 @@
-local status_ok, api = pcall(require, "nvim-tree.api")
 local M = {}
-if not status_ok then
-    M.git_add_toggle = function ()
-    end
-    return M
-end
-
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#git-stage-unstage-files-and-directories-from-the-tree
 function M.git_add_toggle()
-  local node = api.tree.get_node_under_cursor()
-  local gs = node.git_status.file
+    local api = require("nvim-tree.api")
+    local node = api.tree.get_node_under_cursor()
+    local gs = node.git_status.file
 
-  -- If the current node is a directory get children status
-  if gs == nil then
+    -- If the current node is a directory get children status
+    if gs == nil then
     gs = (node.git_status.dir.direct ~= nil and node.git_status.dir.direct[1]) 
          or (node.git_status.dir.indirect ~= nil and node.git_status.dir.indirect[1])
-  end
+    end
 
-  -- If the file is untracked, unstaged or partially staged, we stage it
-  if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
-    vim.cmd("silent !git add " .. node.absolute_path)
+    -- If the file is untracked, unstaged or partially staged, we stage it
+    if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
+        vim.cmd("silent !git add " .. node.absolute_path)
+    -- If the file is staged, we unstage
+    elseif gs == "M " or gs == "A " then
+        vim.cmd("silent !git restore --staged " .. node.absolute_path)
+    end
 
-  -- If the file is staged, we unstage
-  elseif gs == "M " or gs == "A " then
-    vim.cmd("silent !git restore --staged " .. node.absolute_path)
-  end
-
-  api.tree.reload()
-end
+    api.tree.reload()
+    end
 
 return M
