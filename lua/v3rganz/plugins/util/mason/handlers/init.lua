5654a17ff78ada13720f3616873a77843169c691
evgenijdedov <evgggd@gmail.com>
2023-05-24 19:03:59 +0300

    disable pyright diagnostics in favor of better tools

--- lua/v3rganz/plugins/util/mason/handlers/init.lua
+++ lua/v3rganz/plugins/util/mason/handlers/init.lua
@@ -44,6 +44,17 @@ M.handlers = {
     --
     --     lspconfig.pyright.setup(server_opts)
     -- end
+    ['pyright'] = function ()
+        -- disable diagnostics here. all diagnostics are in mypy & ruff
+        local server_opts = get_base_opts()
+        local default_on_attach = server_opts.on_attach
+        server_opts.on_attach = function (client, bufnr)
+            default_on_attach(client, bufnr)
+            local ns = vim.lsp.diagnostic.get_namespace(client.id)
+            vim.diagnostic.disable(nil, ns)
+        end
+        require("lspconfig").pyright.setup(server_opts)
+    end,
     ['lua_ls'] = function ()
         require("lspconfig").lua_ls.setup(override_opts {
             settings = {
