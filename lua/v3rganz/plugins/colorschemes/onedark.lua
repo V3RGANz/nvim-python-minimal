80b9ebd0923ce2dc0502ed72e10bc09451a0fc0f
evgenijdedov <evgggd@gmail.com>
2023-05-25 16:31:45 +0300

    [colorscheme] onedark minors

--- lua/v3rganz/plugins/colorschemes/onedark.lua
+++ lua/v3rganz/plugins/colorschemes/onedark.lua
@@ -9,7 +9,11 @@ function M.factory(style)
     function CS.config()
         local onedark = require("onedark")
         if style == "light" then
+            -- onedark depends on this for determining colors
+            -- so we have to explicitly set it
             vim.o.background = "light"
+        else
+            vim.o.background = "dark"
         end
         onedark.setup({
             style = style,
